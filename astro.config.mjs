28839a6d83107917328e6fca8febfa074cf87b74
Pau Ramon Revilla <masylum@gmail.com>
2024-10-23 11:42:22 +0200

    try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
-    output: 'server',
     plugins: [],
-    adapter: cloudflare({
-        imageService: 'cloudflare',
-    }),
+    adapter: cloudflare({}),
 })
